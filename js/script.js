0120ecd4f9b02e71815af05c2b0534c3adebb493
David Ezenwa <159185092+david-ezenwa@users.noreply.github.com>
2024-03-19 18:47:07 +0000

    made an address JS webpage

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+function enterClicked() {
+  // input
+  const streetnumber = parseInt(document.getElementById("street-number").value)
+  const streetname = document.getElementById("street-name").value
+
+  // output
+  document.getElementById("user-info").innerHTML =
+    "Your address is: " + streetnumber + " " + streetname + "."
+}
